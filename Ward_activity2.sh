4a196a31ab135d7f3db204f68ff27b677bad486a
Bitoliy <86198628+Bitoliy@users.noreply.github.com>
2022-08-10 21:51:47 +0300

    Create Ward_activity2.sh

---
+++ Ward_activity2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function colors {
+  GREEN="\e[32m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+colors
+
+
+echo -e "${GREEN}       _  _  _                _                      _       _           ${NORMAL}"
+echo -e "${GREEN}      | || || |              | |     /\         _   (_)     (_)_         ${NORMAL}"
+echo -e "${GREEN}      | || || | ____  ____ _ | |    /  \   ____| |_  _ _   _ _| |_ _   _ ${NORMAL}"
+echo -e "${GREEN}      | ||_|| |/ _  |/ ___) || |   / /\ \ / ___)  _)| | | | | |  _) | | |${NORMAL}"
+echo -e "${GREEN}      | |___| ( ( | | |  ( (_| |  | |__| ( (___| |__| |\ V /| | |_| |_| |${NORMAL}"
+echo -e "${GREEN}       \______|\_||_|_|   \____|  |______|\____)\___)_| \_/ |_|\___)__  |${NORMAL}"
+echo -e "${GREEN}                                                                  (____/ ${NORMAL}"
