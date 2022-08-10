b62f2a88eaea6ce3aaa885140f349472089db8fc
Bitoliy <86198628+Bitoliy@users.noreply.github.com>
2022-08-10 23:02:08 +0300

    Create Ward_actriv_logo.sh

---
+++ Ward_actriv_logo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function colors {
+  GREEN="\e[32m"
+  RED="\e[39m"
+  NORMAL="\e[0m"
+}
+
+colors
+
+
+echo -e "${GREEN} ${NORMAL}"
+
+echo -e "${GREEN}              /$$      / $$                              /$$        ${NORMAL}"
+echo -e "${GREEN}             | $$      | $$                             | $$        ${NORMAL}"
+echo -e "${GREEN}             | $$      | $$  /$$$$$$ /$$  /$$$$$$   /$$$$$$$        ${NORMAL}"
+echo -e "${GREEN}             | $$ /$$$ | $$ /$$__  $$|$$ /$$__  $$ /$$__  $$        ${NORMAL}"
+echo -e "${GREEN}             | $$$$$$$$$$$$| $$  \ $$$$$| $$  \__/| $$  | $$        ${NORMAL}"
+echo -e "${GREEN}             | $$$$$  $$$$$| $$  | $$$$$| $$      | $$  | $$        ${NORMAL}"
+echo -e "${GREEN}             \  $$$  \ $$$/|  $$$$$$/|$$| $$      |  $$$$$$$        ${NORMAL}"
+echo -e "${GREEN}              \___/   \__/  \______/ \_||__/       \_______/        ${NORMAL}"
+echo -e "${GREEN}                                                                    ${NORMAL}"
+echo -e "${GREEN}      / $$      /$$       /$$   /$$           /$$   /$$   /$$   /$$ ${NORMAL}"
+echo -e "${GREEN}     / $$$$    | $$ /$$  | $$   \_/           \_/  | $$  | $$  | $$ ${NORMAL}"
+echo -e "${GREEN}    / $$_ $$   | $$$$   /$$$$$$ /$$/ $$  / $$ /$$ /$$$$$$| $$  | $$ ${NORMAL}"
+echo -e "${GREEN}   / $$$$$$$$  | $$\ $$  | $$  | $$\ $$ / $$ | $$  | $$  | $$  | $$ ${NORMAL}"
+echo -e "${GREEN}  / $$    \ $$ | $$ \ $$ | $$  | $$ \ $$ $$  | $$  | $$  |  $$$$$$$ ${NORMAL}"
+echo -e "${GREEN} / $$      \ $$| $$  \ $$| $$$ | $$  \ $$$   | $$  | $$$  \_____|$$ ${NORMAL}"
+echo -e "${GREEN}/__/        \__|\__/  \__|___/ |__/   \___|  |__/  |___/         $$ ${NORMAL}"
+echo -e "${GREEN}                                                        /$$      $$ ${NORMAL}"
+echo -e "${GREEN}                                                        | $$$$$$$$  ${NORMAL}"
+echo -e "${GREEN}                                                         \_______/  ${NORMAL}"
